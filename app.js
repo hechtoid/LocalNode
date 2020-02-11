0364029f3ac212494b23a7d7910dd2cfba24818c
hechtoid <hechtsp@gmail.com>
2020-02-11 00:16:33 -0800

    add react

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ const serveIndex = require('serve-index-sort-date');
 const expressip = require('express-ip');
 app.use(expressip().getIpInfoMiddleware);
 
+const weather = require("./routes/api/weather")
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get("/", function (req, res) {
   const ipInfo = req.ipInfo;
   console.log(`ROOT ${req.ip} [${ipInfo.city}] accessed ROOT at ${new Date().toUTCString()} ROOT`);
   // res.sendStatus(304);
 })
+
+app.use("/api/weather", weather);
 
 app.use(express.static(path.join(__dirname, 'static/weather/build')));
 app.get('/weather', function (req, res) {
