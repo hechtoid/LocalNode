ad6c9fc6d14b4fbbafd787be8340f4d8fb1534f6
hechtoid <hechtsp@gmail.com>
2020-02-14 01:29:59 -0800

    transit panes

--- frontend/src/components/transit.js
+++ frontend/src/components/transit.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Union from './union';
 import Stockton from './stockton';
 import Sansome from './sansome';
@@ -6,17 +6,24 @@ import VanNess from './vanNess';
 import Broadway from './broadway';
 
 function Transit() {
+  let [pane, setPane] = useState('marin');
   return (
-      <div className="transit">
-      <div className="small">
-        <Union />
-        <Sansome />
-	<Broadway />
+    <div>
+      <div className="transit-switcher">
+        <div className={pane === 'marin' ? 'transit-switch-on' : 'transit-switch-off'} onClick={() => setPane('marin')}>MARIN</div>
+        <div className={pane === 'frisco' ? 'transit-switch-on' : 'transit-switch-off'} onClick={() => setPane('frisco')}>FRISCO</div>
       </div>
-      <div className="big">
-        <Stockton />
+    <div className="transit">
+        <div className={pane === 'marin' ? 'transit-on' : 'transit-off'}>
+        <Sansome />
         <VanNess />
       </div>
+        <div className={pane === 'frisco' ? 'transit-on' : 'transit-off'}>
+        <Stockton />
+        <Union />
+        <Broadway />
+      </div>
+    </div>
     </div>
   );
 }
