426884f174ce6bac83a927a2a840a95d6b07e42a
hechtoid <hechtsp@gmail.com>
2020-03-03 02:24:43 -0800

    working vehicular form

--- frontend/src/components/transit/vehicular.js
+++ frontend/src/components/transit/vehicular.js
@@ -4,50 +4,64 @@ import axios from 'axios';
 class Vehicular extends React.Component {
     constructor(props) {
         super(props);
-        this.state = this.props.location.state || {busNumber: 8816}
+        this.state = this.props.location.state || {vehicleNumber: 8612}
            
         
-        this.loadBus = this.loadBus.bind(this)
-        this.updateBusNumber = this.updateBusNumber.bind(this)
+        this.loadVehicle = this.loadVehicle.bind(this)
+        this.updateVehicleNumber = this.updateVehicleNumber.bind(this)
     }
 
     componentDidMount() {
-        this.loadBus()
+        this.loadVehicle()
     }
-    loadBus() { 
-        axios.get(`http://api.511.org/transit/VehicleMonitoring?api_key=72939361-85f9-4019-aa55-d62e4e7e2e59&agency=SF&format=json&vehicleID=${this.state.busNumber}`)
+    loadVehicle() { 
+        let vehicle = {}
+        axios.get(`http://api.511.org/transit/VehicleMonitoring?api_key=72939361-85f9-4019-aa55-d62e4e7e2e59&agency=SF&format=json&vehicleID=${this.state.vehicleNumber}`)
             .then(res => {
-                let bus = res.data.Siri.ServiceDelivery.VehicleMonitoringDelivery.VehicleActivity[0].MonitoredVehicleJourney
-                this.setState({ bus });
+                vehicle = res.data.Siri.ServiceDelivery.VehicleMonitoringDelivery.VehicleActivity
+                ? res.data.Siri.ServiceDelivery.VehicleMonitoringDelivery.VehicleActivity[0].MonitoredVehicleJourney
+                : {}
+                this.setState({ vehicle });
             })
     }
 
-    updateBusNumber() {
+    updateVehicleNumber() {
         return e => {
-            let busNumber = e.currentTarget.value
+            let vehicleNumber = e.currentTarget.value
             this.setState({
-                busNumber
+                vehicleNumber
             })
         }
     }
     render() {
-            let busInfo = 'no Tracked Vehicle'
-            if (this.state.bus) {
-                let gmapsURL = `https://www.google.com/maps/search/?api=1&query=${this.state.bus.VehicleLocation.Latitude},${this.state.bus.VehicleLocation.Longitude}`
-                busInfo = <div className="busInfo">
-                        Bus <a href = {gmapsURL} target="_blank" rel="noopener noreferrer">
-                        #{this.state.bus.VehicleRef}</a> is currently running the  
-                        <span className="bold"> {this.state.bus.LineRef} </span> 
-                        to <span className="bold"> {this.state.bus.DestinationName}</span>.
+            let vehicleInfo = <div>no Tracked Vehicle</div>
+            let key = 0
+            if (this.state.vehicle && this.state.vehicle.Monitored) {
+                let gmapsURL = `https://www.google.com/maps/search/?api=1&query=${this.state.vehicle.VehicleLocation.Latitude},${this.state.vehicle.VehicleLocation.Longitude}`
+                vehicleInfo = <div className="vehicleInfo">
+                        Vehicle <span className="bold">#{this.state.vehicle.VehicleRef}</span> is running route  
+                        <span className="bold"> {this.state.vehicle.LineRef} </span> 
+                        to <span className="bold"> {this.state.vehicle.DestinationName}</span>.
                         <br></br>
-                        The NextStop is <span className="bold">{this.state.bus.MonitoredCall.StopPointName}</span>.
+                        The NextStop is <span className="bold">{this.state.vehicle.MonitoredCall.StopPointName}</span>,
+                        <br></br>
+                        Scheduled for <span className="bold">
+                            {new Date(Date.parse(this.state.vehicle.MonitoredCall.AimedArrivalTime)).toLocaleTimeString()} </span>
+                            but arriving at <span className="bold">
+                                {new Date(Date.parse(this.state.vehicle.MonitoredCall.ExpectedArrivalTime)).toLocaleTimeString()}
+                            </span>
+                        <br></br>
+                        <a href = {gmapsURL} target="_blank" rel="noopener noreferrer">
+                            Last Reported GPS Coördinates
+                        </a>
                         <br></br>
                         Future Stops are: 
                         <br></br>
-                        {this.state.bus.OnwardCalls.OnwardCall.map(stop => {
+                        {this.state.vehicle.OnwardCalls.OnwardCall.map(stop => {
                             return (
-                                <div>
-                                stop.StopPointName
+                                <div className="bus" key={key++}>
+                                {stop.StopPointName}
+                                <br></br>
                                 <span className="gray">
 {new Date(Date.parse(stop.AimedArrivalTime)).toLocaleTimeString()}
         </span>
@@ -60,14 +74,19 @@ class Vehicular extends React.Component {
                 </div>
             }
         return (
-            <div>foo
+            <div>
+                Enter the <a href="https://www.sfmta.com/getting-around/muni/muni-feedback" target="_blank" rel="noopener noreferrer">
+                Vehicle Number:</a>
+                <form onSubmit={this.loadVehicle}>
             <input type="text"
-                    id="bus-number"
-                    placeholder="Bus Number"
-                    value={this.state.busNumber}
-                    onChange={this.updateBusNumber()}
-                />
-            {busInfo}
+                    id="vehicle-number"
+                    placeholder="Vehicle Number"
+                    value={this.state.vehicleNumber}
+                    onChange={this.updateVehicleNumber()}
+                /> <br></br>
+                <input type="submit" value="Track Vehicle" />
+            </form>
+            {vehicleInfo}
             </div>
         );
     }
