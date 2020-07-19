3104b7ef23425b9d8e289feda463efd63fa595ab
hechtoid <hechtsp@gmail.com>
2020-07-19 01:54:57 -0700

    default status for mpc

--- routes/control/mpc.js
+++ routes/control/mpc.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const shell = require('shelljs')
 
+router.get('/', (req, res) => { 
+    const { stdout, stderr, code } = shell.exec(`mpc`, { silent: true })
+    res.send(stdout)
+})
 router.get('/:cmd?', (req, res) => {
     const { stdout, stderr, code } = shell.exec(`mpc ${req.params.cmd}`, { silent: true })
     res.send(stdout)
