7dc3553bd28ff6cc09228aadc064a3797b581bf3
hechtoid <hechtsp@gmail.com>
2020-07-19 01:49:35 -0700

    reshrink mpc

--- routes/control/mpc.js
+++ routes/control/mpc.js
@@ -2,27 +2,9 @@ const express = require("express");
 const router = express.Router();
 const shell = require('shelljs')
 
-router.get('/single', (req, res) => {
-    res.send(shell.exec('mpc single'))
-})
-router.get('/status', (req, res) => {
-    // res.send(shell.exec('mpc'))
-    const { stdout, stderr, code } = shell.exec('mpc', { silent: true })
+router.get('/:cmd?', (req, res) => {
+    const { stdout, stderr, code } = shell.exec(`mpc ${req.params.cmd}`, { silent: true })
     res.send(stdout)
-})
-router.get('/', (req, res) => {
-    res.send(shell.exec('mpc'))
-})
-router.get('/play', (req, res) => {
-    res.send(shell.exec('mpc play'))
-})
-router.get('/stop', (req, res) => {
-    res.send(shell.exec('mpc stop'))
-})
-
-router.get('/mpcstop/', (req, res) => {
-    shell.exec('mpc stop >/dev/null 2>&1')
-    res.sendStatus(204)
 })
 
 module.exports = router;
