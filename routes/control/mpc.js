f9e62f89d885157297ebc7922696c5be6e670e3b
hechtoid <hechtsp@gmail.com>
2020-07-19 03:27:29 -0700

    introduce gaping security hole

--- routes/control/mpc.js
+++ routes/control/mpc.js
@@ -6,9 +6,9 @@ router.get('/', (req, res) => {
     const { stdout, stderr, code } = shell.exec(`mpc`, { silent: true })
     res.send(stdout)
 })
-router.get('/:cmd?', (req, res) => {
-    const { stdout, stderr, code } = shell.exec(`mpc ${req.params.cmd}`, { silent: true })
-    res.send(stdout)
-})
+// router.get('/:cmd?', (req, res) => {
+//   const { stdout, stderr, code } = shell.exec(`mpc ${req.params.cmd}`, { silent: true })
+//    res.send(stdout)
+//})
 
 module.exports = router;
